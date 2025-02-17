65003d4dcaf44a3c3123750e6f9f4786ebb7e928
Devashish-n17 <devashish.revadkar@nitorinfotech.com>
2025-02-17 16:10:38 +0530

    simple changes

--- server/app/main.py
+++ server/app/main.py
@@ -4,15 +4,27 @@ from .apis.api import app
 import uvicorn
 
 # Configure CORS
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],  # Explicitly include OPTIONS
+#     allow_headers=["*"],
+#     expose_headers=["*"],
+#     max_age=600,  # Cache preflight requests for 10 minutes
+# )
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:4200"],
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # Explicitly include OPTIONS
+    allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=["*"],
-    max_age=600,  # Cache preflight requests for 10 minutes
 )
-
+ 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="VM-021-SPA.NITORINFOTECH.IN", port=8001)
